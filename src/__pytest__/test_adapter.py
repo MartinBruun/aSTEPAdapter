d107ffdb71764cd2d343e3090993ff19fd050b1f
MartinBruun <mbm93fen@gmail.com>
2021-10-07 21:38:10 +0200

    change from_json test

--- src/__pytest__/test_adapter.py
+++ src/__pytest__/test_adapter.py
@@ -3,18 +3,14 @@ import json
 
 from src.adapter import Adapter
 
-@pytest.mark.parametrize("test_dict", [
-    (None),
-    ({}),
-])
-def test_adapter_from_json_deserializes_options_for_creator(test_dict):
-    expected_options_dict = test_dict
-    serialized_test_json = json.dumps(expected_options_dict)
+# Should be changed to get a json string as input, when options gets a clear usecase
+def test_adapter_from_json_deserializes_empty_options_for_creator():
+    serialized_test_json = json.dumps({})
     adapter = Adapter()
 
     deserialized_options = adapter.from_json(serialized_test_json)
 
-    assert deserialized_options == expected_options_dict
+    assert {} == deserialized_options
 
 def test_adapter_to_json_serializes_grouping_1_to_traffic_model_schema(fake_mapdata_node_1, fake_mapdata_edge_1, fake_expected_model_1):
     adapter = Adapter()
